79b98681d9628dfc06e9ac0ffaa2fc935b52c620
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2024-02-24 19:43:47 +0100

    Do some practice

---
+++ exercises/auto_keyword/auto_keyword.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	auto i{42};                       // Type of i is deduced as int
+	auto str1 = "Hello";              // Type of str1 is deduced as const char*
+	auto str2 = "Hello"s;             // Type of str2 is deduced as std::string
+
+	cout << "i * 2 = " << i * 2 << endl;
+	auto it = str2.begin();
+	cout << "First element of str2: " << *it << endl;
+}
+
+/* Auto as a function */
+int func() {                 // Function returning reference to const int
+	return 5;
+}
+
+int main() {
+	auto x = func();                // x has type int
+	cout << x << endl;
+}
