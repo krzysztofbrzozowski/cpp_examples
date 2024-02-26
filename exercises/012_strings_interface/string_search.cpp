ca456ecd997345cac8be7ea42b2efc50fb1a99af
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2024-02-26 19:10:19 +0100

    Added finding methods

--- exercises/012_strings_interface/string_search.cpp
+++ exercises/012_strings_interface/string_search.cpp
@@ -36,7 +36,22 @@ void reverse_find(void) {
     }
 }
 
+void regular_find_first_of(void) {
+    string str{"Hello world"};
+    string vowels{"aeiou"};
+    
+    size_t pos = str.find_first_of(vowels);
+
+    if(pos != string::npos) {
+        cout << R"(Fist occurence one of 'a e i o u' is at index: )" << pos << endl;
+    } else {
+        cout << R"(Could not find 'a e i o u'  in the sring)" << endl;
+    }
+}
+
+
 int main() {
     regular_find();
     reverse_find();
+    regular_find_first_of();
 }
